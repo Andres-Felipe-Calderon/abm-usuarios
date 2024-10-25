6329855fce7ddc9f6f388af6b470d0788868cb5e
Andres-Felipe-Calderon <afelipecalderon@unicesar.edu.co>
2024-10-25 14:55:15 -0500

    Bienvenidos

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,10 +17,11 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot()
-    {
-        if (env('APP_ENV') === 'production') {
-            URL::forceScheme('https');
-        }
-    }
+   
+public function boot()
+{
+    if(env( key: 'APP_ENV') !== 'local') {
+        URL::forceScheme(scheme: 'https');
+      }
+}
 }
